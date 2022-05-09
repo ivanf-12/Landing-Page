d1ca0064516cc8a24c62acc6c4e4faa3c27ab00c
ivanf-12 <febriantoivan12@gmail.com>
2022-05-09 21:06:32 +0700

    make header

---
+++ script.js
@@ -0,0 +1,15 @@
+const menu_bar = document.querySelector("#menu-bar");
+const nav_bar = document.querySelector("#nav-bar")
+
+menu_bar.addEventListener('click', ()=>{
+  if(nav_bar.classList.contains("active")){
+    nav_bar.classList.remove("active");
+    menu_bar.classList.remove("fa-xmark");
+    menu_bar.classList.add("fa-bars");
+  }
+  else{
+   menu_bar.classList.remove("fa-bars");
+   menu_bar.classList.add("fa-xmark");
+   nav_bar.classList.add("active");
+ }
+});
